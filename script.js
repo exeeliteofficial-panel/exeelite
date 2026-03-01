b1d7486afb05b95265fb2ceee7e7909c1ff13f42
exeeliteofficial-panel <exeeliteofficial@gmail.com>
2026-03-01 14:07:58 +0600

    Update script.js

--- script.js
+++ script.js
@@ -710,6 +710,19 @@ function showOnlineBuy() {
 function goToPayment(link) {
   window.location.href = link;
 }
+function toggleOnlineOptions() {
+    const box = document.getElementById("onlineOptions");
+    if (box.style.display === "none") {
+        box.style.display = "block";
+    } else {
+        box.style.display = "none";
+    }
+}
+
+function goToPayment(link) {
+    window.open(link, "_blank");
+}
+
 
 function toggleOnlineOptions() {
     const box = document.getElementById("onlineOptions");
