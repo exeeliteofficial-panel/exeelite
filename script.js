4a7f0e933a114519a88ce5f83fe94f340b2ef27c
exeeliteofficial-panel <exeeliteofficial@gmail.com>
2026-03-06 20:43:31 +0600

    Update script.js

--- script.js
+++ script.js
@@ -217,24 +217,3 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-const counters = document.querySelectorAll('.stat-number');
-
-counters.forEach(counter => {
-    counter.innerText = '0';
-
-    const updateCounter = () => {
-        const target = +counter.getAttribute('data-target');
-        const current = +counter.innerText;
-
-        const increment = target / 200;
-
-        if(current < target){
-            counter.innerText = Math.ceil(current + increment);
-            setTimeout(updateCounter, 10);
-        } else {
-            counter.innerText = target;
-        }
-    };
-
-    updateCounter();
-});
